1410ba9db7c24fd1ddf58c27bcc5ca086025c660
Mark Liman <65871508+StacLigasfolf@users.noreply.github.com>
2023-03-05 17:26:30 +0300

    GUI is ready and loaded
    made and added all the necessary scenes, models, also redefined the functions of loading, processing and updating graphics along with the loading screen

--- elven-scout/src/game.js
+++ elven-scout/src/game.js
@@ -1,19 +1,39 @@
 import { Screen } from "./screen";
 import { Loading } from "./scenes/loding";
+import { Menu } from "./scenes/menu";
+import { Scene } from "./scene";
 
 export class Game {
-    constructor(){
-        this.screen = new Screen(1920, 1080);
+    constructor({width = 1000, height = 1000} = {}){
+        this.screen = new Screen(width, height);
+        this.screen.loadImages({
+            player: 'img/hero.png',
+            enemy: 'img/enemy.png',
+            tiles: 'img/scene_1.png',
+            title: "img/title.png"
+        });
         this.scenes = {
-            loading: new Loading(this)
+            loading: new Loading(this),
+            menu: new Menu(this)
         };
         this.currentScene = this.scenes.loading;
         this.currentScene.init();
     }
 
+    changeScene(status) {
+        switch (status) {
+            case Scene.LOADED:
+                return this.scenes.menu;
+                break;
+
+            default:
+                return this.scenes.menu;
+        }
+    }
+
     frame(time){
-        if(this.currentScene.isActive == false){
-            this.currentScene = this.scenes[this.currentScene.nextScene];
+        if(this.currentScene.status != Scene.WORKING){
+            this.currentScene = this.changeScene(this.currentScene.status);
             this.currentScene.init();
         }
         this.currentScene.render(time);
